e8278d84b90783964fe5b36d9b996be0f6ce9f83
Frey <CyberFrey@pm.me>
2021-08-02 22:31:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import telebot
+import time
 
+from PIL import Image
+import requests
+from io import BytesIO
 
 
 bot = telebot.TeleBot("1910033477:AAFgTbZ6E-_KcgjEoQNpoPDSegx_ZGjy3Pk", parse_mode=None) # You can set parse_mode by default. HTML or MARKDOWN
@@ -7,6 +11,13 @@ bot = telebot.TeleBot("1910033477:AAFgTbZ6E-_KcgjEoQNpoPDSegx_ZGjy3Pk", parse_mo
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
 	bot.reply_to(message, "Howdy, how are you doing?")
+	
+	
+@bot.message_handler(commands=["kiss"])
+def sticker(message):
+    bot.send_sticker(
+        message.chat.id, "CAACAgUAAxkBAAECrIthCCQ-foazfBmWjVt21cgBZkryPgAC-wEAAqdCQVfssteQiArdhCAE")
+
 	
 
 
